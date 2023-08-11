4b51a056179fcc84e74bab2301984b9602a09142
Izabela K <aizeilla12344@gmail.com>
2023-08-11 17:27:44 +0200

    Fixed gameover screen

--- FlappyUfo/Assets/UI/GameOver/GameOverScreen.cs
+++ FlappyUfo/Assets/UI/GameOver/GameOverScreen.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public TextMeshProUGUI PointScore;
     public TextMeshProUGUI BestScore;
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+        audioManager.PlaySoundEffects(audioManager.deathSound);
+    }
+    public void RestartButton()
+    {
+        LoadAgain();
+    }
     public void ShowGameOverScreen(int pointScore, int bestScore)
     {
         gameObject.SetActive(true);
         PointScore.text = $"{pointScore} points";
         BestScore.text = $"BEST SCORE:\n{bestScore}";
+    }
+
+    public void LoadAgain()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
     }
 }
